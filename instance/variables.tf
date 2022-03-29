d488963108f3f6006a39ded841f44b022727a5ed
ailyn-perez201 <102550974+ailyn-perez201@users.noreply.github.com>
2022-03-29 15:48:31 -0500

    Update variables.tf

--- instance/variables.tf
+++ instance/variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key" {
-  default = "AKIAVN76YS2SSFSYKIXT"
-}
-
-variable "aws_secret_key" {
-  default = "kyqV7EmEKImr1YR0QuybrTVTeWiqF08UBiotODJP"
-}
-
-variable "region" {
-  default = "us-east-1"
-}
-
 variable "instance_type" {
 }
 
